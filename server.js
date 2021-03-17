aae85f1ef77322e1a77bf7abcb649b6926b1104d
Sean Morrissey <semorrissey17@gmail.com>
2021-03-17 16:28:03 -0400

    updated file reading code to read the entire directory + clear db

--- server.js
+++ server.js
@@ -26,9 +26,6 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/webpage.html");
 });
 
-app.get("/libs/require.js", function(req, res) {
-  res.sendFile(__dirname + "/libs/require.js");
-})
 
 //mongodb connection setup
 const MongoClient = require('mongodb').MongoClient;
@@ -41,45 +38,69 @@ const dbClient = new MongoClient(uri, {
 
 //reads file into array and converts to JSON
 
-function reading() {
-  fs.readFile(__dirname + '/files/VIIRS_I_SouthEast_Asia_VNP14IMGTDL_NRT_2021068.txt', 'utf8', function read(err, data) {
-    if (err) throw err;
-    const info = data.toString();
-    var array = info.split("\n");
-    var jsonKeys = array[0].split(",");
-    array.splice(0, 1);
-    var jsonResult = new Array;
-    for (i in array) {
-      var temp = array[i].split(",");
-      var json = {};
-      for (j in temp) {
-        json[jsonKeys[j]] = temp[j];
-      }
-      jsonResult.push(json);
+function readFiles(dirname, onFileContent, onError) {
+  fs.readdir(dirname, function(err, filenames) {
+    if (err) {
+      onError(err);
+      return;
     }
-    for (k = 0; k < jsonResult.length; k += 100) {
-      var payload = new Array;
-      var temp = jsonResult.slice(k, k + 100);
-      for (l = 0; l < 100; l++) {
-        payload.push(temp[l]);
-      }
-      /*fetch("https://maepingfirepa.herokuapp.com/push", {
-        method: "POST",
-        body: JSON.stringify(payload),
-        headers: {
-          "Content-Type": "application/json"
+    filenames.forEach(function(filename) {
+      fs.readFile(dirname + filename, 'utf-8', function(err, content) {
+        if (err) {
+          onError(err);
+          return;
         }
-      });*/
-    }
-    fetch("https://firmsnrtdownloader.herokuapp.com/push", {
-      method: "POST",
-      body: JSON.stringify(jsonResult),
-      headers: {
-        "Content-Type": "application/json"
-      }
+        onFileContent(filename, content);
+      });
     });
   });
 }
+
+function pushData(fname, data) {
+  const info = data.toString();
+  var array = info.split("\n");
+  var jsonKeys = array[0].split(",");
+  array.splice(0, 1);
+  var jsonResult = new Array;
+  for (i in array) {
+    var temp = array[i].split(",");
+    var json = {};
+    for (j in temp) {
+      json[jsonKeys[j]] = temp[j];
+    }
+    jsonResult.push(json);
+  }
+  fetch("https://firmsnrtdownloader.herokuapp.com/push", {
+    method: "POST",
+    body: JSON.stringify(jsonResult),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+}
+// fetch from source apis
+
+async function csvDownload() {
+  const url = 'https://nrt3.modaps.eosdis.nasa.gov/api/v2/content/archives/FIRMS/README.pdf'; // link to file you want to download
+  const path = "/public/tmp/" // where to save a file
+
+  const downloadFile = (async (url, path) => {
+    const res = await fetch(url, {
+      headers: {
+        'Authorization': 'Bearer bWFlcGluZ25vZmlyZTpiV0ZsY0dsdVoyNXZabWx5WlVCbmJXRnBiQzVqYjIwPToxNjE1MjUyMTUxOmEwZTc5OTg4YzI2Yjg5ZTMxZWViYzFlOGI5MzQ4MGFkMzVmNTQwNzQ'
+      }
+    });
+    const fileStream = fs.createWriteStream(path);
+    await new Promise((resolve, reject) => {
+      res.body.pipe(fileStream);
+      res.body.on("error", reject);
+      fileStream.on("finish", resolve);
+    });
+  });
+  console.log("i worked");
+  return true;
+}
+
 
 //requests from database
 
@@ -93,7 +114,9 @@ async function addDocs(info) {
     const options = {
       ordered: true
     };
-
+    //remove old info for new info
+    await dbClient.collection.deleteMany({});
+    //post new info
     const result = await collection.insertMany(info);
     console.log(result.insertedCount + 'documents were inserted');
   } finally {
@@ -106,7 +129,9 @@ app.post("/push", bodyParser.json(), function(req, res) {
 });
 
 app.post("/recieve", function(req, res) {
-  reading();
+  readFiles(__dirname, pushData, function(err) {
+    if (err) throw err;
+  });
   //res.setStatusCode(200);
 })
 

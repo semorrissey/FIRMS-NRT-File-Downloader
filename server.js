a2426624d3c43c37e63fc60efdaf83f00c6997c4
Sean Morrissey <semorrissey17@gmail.com>
2021-03-16 21:11:23 -0400

    added functionality

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/webpage.html");
 });
 
+app.get("/libs/require.js", function(req, res) {
+  res.sendFile(__dirname + "/libs/require.js");
+})
+
 //mongodb connection setup
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://admin:${process.env.DB_PASSWORD}@cluster0.em7pv.mongodb.net/FireData?retryWrites=true&w=majority`;
@@ -34,7 +38,47 @@ const dbClient = new MongoClient(uri, {
   useUnifiedTopology: true
 });
 
+//reads file into array and converts to JSON
 
+function reading() {
+  fs.readFile(__dirname + '/files/VIIRS_I_SouthEast_Asia_VNP14IMGTDL_NRT_2021068.txt', 'utf8', function read(err, data) {
+    if (err) throw err;
+    const info = data.toString();
+    var array = info.split("\n");
+    var jsonKeys = array[0].split(",");
+    array.splice(0, 1);
+    var jsonResult = new Array;
+    for (i in array) {
+      var temp = array[i].split(",");
+      var json = {};
+      for (j in temp) {
+        json[jsonKeys[j]] = temp[j];
+      }
+      jsonResult.push(json);
+    }
+    for (k = 0; k < jsonResult.length; k += 100) {
+      var payload = new Array;
+      var temp = jsonResult.slice(k, k + 100);
+      for (l = 0; l < 100; l++) {
+        payload.push(temp[l]);
+      }
+      /*fetch("https://maepingfirepa.herokuapp.com/push", {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });*/
+    }
+    fetch("http://localhost:3000/push", {
+      method: "POST",
+      body: JSON.stringify(jsonResult),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+  });
+}
 
 //requests from database
 
@@ -57,10 +101,14 @@ async function addDocs(info) {
 }
 
 app.post("/push", bodyParser.json(), function(req, res) {
-  addDocs(JSON.parse(req.body)).catch(console.dir);
+  console.log(req.body);
+  addDocs(req.body).catch(console.dir);
 });
 
-
+app.post("/recieve", function(req, res) {
+  reading();
+  //res.setStatusCode(200);
+})
 
 
 

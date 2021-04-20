4e75bd6eaa1abbae0efa3c75cd06516208cee026
semorrissey <semorrissey@wpi.edu>
2021-04-20 20:29:33 +0000

    cleaned up code and edited server file to work with digitial ocean.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 app.use(express.static('public'));
 app.use(bodyParser.json({
@@ -36,9 +36,8 @@ const dbClient = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-
-//download nasa files to /files/ directory using perl script
-
+dbClient.connect().then(() => console.log('MongoDB connected...'))
+      .catch(err => console.log(err));;
 
 //reads file into array and converts to JSON
 
@@ -72,7 +71,7 @@ function pushData(fname, data) {
     }
     jsonResult.push(json);
   }
-  fetch("https://firmsnrtdownloader.herokuapp.com/push", {
+  fetch("http://188.166.191.81:8000/push", {
     method: "POST",
     body: JSON.stringify(jsonResult),
     headers: {
@@ -80,55 +79,10 @@ function pushData(fname, data) {
     }
   });
 }
-// fetch from source apis
-
-async function csvDownload() {
-  const url = 'https://nrt3.modaps.eosdis.nasa.gov/api/v2/content/archives/FIRMS/README.pdf'; // link to file you want to download
-  const path = "/public/tmp/" // where to save a file
-
-  const downloadFile = (async (url, path) => {
-    const res = await fetch(url, {
-      headers: {
-        'Authorization': 'Bearer bWFlcGluZ25vZmlyZTpiV0ZsY0dsdVoyNXZabWx5WlVCbmJXRnBiQzVqYjIwPToxNjE1MjUyMTUxOmEwZTc5OTg4YzI2Yjg5ZTMxZWViYzFlOGI5MzQ4MGFkMzVmNTQwNzQ'
-      }
-    });
-    const fileStream = fs.createWriteStream(path);
-    await new Promise((resolve, reject) => {
-      res.body.pipe(fileStream);
-      res.body.on("error", reject);
-      fileStream.on("finish", resolve);
-    });
-  });
-  console.log("i worked");
-  return true;
-}
-
-app.get('/download', callDownloadScript);
-
-function callDownloadScript(req, res) {
-
-  var spawn = require("child_process").spawnSync('python', ["./laads-data-download.py",
-    req.query.source,
-    req.query.destination,
-    req.query.token
-  ]);
-  console.log(spawn.stdout);
-}
-
-async function fetchDownload() {
-  await fetch("https://firmsnrtdownloader.herokuapp.com/download?source=https%3A%2F%2Fnrt3.modaps.eosdis.nasa.gov%2Fapi%2Fv2%2Fcontent%2Farchives%2FFIRMS%2Fviirs%2FSouthEast_Asia&destination=files%2F&token=bWFlcGluZ25vZmlyZTpiV0ZsY0dsdVoyNXZabWx5WlVCbmJXRnBiQzVqYjIwPToxNjE2MDE2Nzg4OmQzYjY0ZmNlNTBjYWI2ZDYzZmU3YTUxYWM2ZjVmOTc3MWIwN2FhOWY", {
-    method: "GET"
-  });
-}
-
 
 //requests from database
 
 async function addDocs(info) {
-  try {
-    await dbClient.connect().then(() => console.log('MongoDB connected...'))
-      .catch(err => console.log(err));;
-
     const collection = dbClient.db("FireData").collection("NasaFirmsData");
 
     const options = {
@@ -138,10 +92,7 @@ async function addDocs(info) {
     await collection.deleteMany({});
     //post new info
     const result = await collection.insertMany(info);
-    console.log(result.insertedCount + 'documents were inserted');
-  } finally {
-    await dbClient.close();
-  }
+    console.log(result.insertedCount + 'documents were inserted'); 
 }
 
 app.post("/push", bodyParser.json(), function(req, res) {
@@ -149,9 +100,7 @@ app.post("/push", bodyParser.json(), function(req, res) {
 });
 
 app.post("/recieve", async function(req, res) {
-  //await fetchDownload();
-  readFiles(__dirname + "/files/", pushData);
-  //res.setStatusCode(200);
+  readFiles("/root/FIRMS/viirs/SouthEast_Asia/", pushData);
 })
 
 
